9392b437505de55b421ae98f98a23abaec49ce21
Almantas <internet622@gmail.com>
2024-05-30 12:21:00 +0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,17 @@
-export interface ID {
-  id: number;
-}
 export interface IPost {
   id: number;
   name: string;
 }
+export interface PostItemProps {
+  posts: IPost[];
+  updatePost: UpdatePost;
+}
+export interface PostFormProps {
+  addPost: AddPost;
+  edit?: { id: number; value: string };
+}
 
-export type EditPost = (newTodo: string) => void;
+export type EditPost = (newPost: string) => void;
 export type RemovePost = (id: number) => void;
-export type AddPost = (newTodo: string) => void;
+export type AddPost = (newPost: string) => void;
 export type UpdatePost = (id: number, name: string) => void;
