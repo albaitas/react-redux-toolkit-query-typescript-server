1034fd05f68408a6863e4b34822c80cad9a81d25
Almantas <internet622@gmail.com>
2024-05-30 12:22:11 +0300

    Update postsApi.ts

--- src/redux/postsApi.ts
+++ src/redux/postsApi.ts
@@ -1,32 +1,32 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { ID } from './../types';
+import { IPost } from './../types';
 
 export const postsApi = createApi({
   reducerPath: 'postsApi',
   tagTypes: ['Posts'],
   baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3001/' }),
   endpoints: (build) => ({
-    getAllPosts: build.query({
+    getAllPosts: build.query<IPost[], void>({
       query: () => `posts`,
-      providesTags: (result) => (result ? [...result.map(({ id }: ID) => ({ type: 'Posts' as const, id })), { type: 'Posts', id: 'LIST' }] : [{ type: 'Posts', id: 'LIST' }])
+      providesTags: ['Posts']
     }),
 
-    addPost: build.mutation({
+    addPost: build.mutation<void, string>({
       query: (body) => ({
         url: `posts`,
         method: 'POST',
         body
       }),
-      invalidatesTags: [{ type: 'Posts', id: 'LIST' }]
+      invalidatesTags: ['Posts']
     }),
-    removePost: build.mutation({
+    removePost: build.mutation<void, number>({
       query: (id) => ({
         url: `posts/${id}`,
         method: 'DELETE'
       }),
       invalidatesTags: ['Posts']
     }),
-    updatePost: build.mutation({
+    updatePost: build.mutation<void, IPost>({
       query: (post) => ({
         url: `posts/${post.id}`,
         method: 'PUT',
